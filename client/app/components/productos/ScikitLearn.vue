00c899616240b86f164c437a4a7dba95b0a2724d
Omar97perez <omarperezznakar@gmail.com>
2020-05-26 23:17:48 +0100

    Algoritmos: Estructura básica acabada

--- client/app/components/productos/ScikitLearn.vue
+++ client/app/components/productos/ScikitLearn.vue
@@ -10,6 +10,10 @@
           </div>
         </div>
 
+        <div class="mt-5" align="center">
+          <h2><strong>Clasificación</strong></h2>
+        </div>
+
         <div class="row mt-5">
           <div class="card col-4" style="border: 1px solid #343a40!important">
             <img class="card-img-top" src="/img/ScikitLearn.png" alt="Card image cap">
@@ -22,15 +26,44 @@
             </div>
           </div>
         </div>
-        
+
+        <div class="mt-5" align="center">
+          <h2><strong>Regresión</strong></h2>
+        </div>
+
+        <div class="row mt-5">
+          <div class="card col-4" style="border: 1px solid #343a40!important">
+            <img class="card-img-top" src="/img/ScikitLearn.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Scikit Learn</h5>
+              <p class="card-text">Use la librería de Scikit Learn en la nube.</p>
+              <div class="card-footer mt-2" align="center" style="position: relative!important; border-top: 1px solid  #343a40!important;">
+                <button class="btn btn-dark">Acceder</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="mt-5" align="center">
+          <h2><strong>Clustering</strong></h2>
+        </div>
+
+        <div class="row mt-5">
+          <div class="card col-4" style="border: 1px solid #343a40!important">
+            <img class="card-img-top" src="/img/ScikitLearn.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Scikit Learn</h5>
+              <p class="card-text">Use la librería de Scikit Learn en la nube.</p>
+              <div class="card-footer mt-2" align="center" style="position: relative!important; border-top: 1px solid  #343a40!important;">
+                <button class="btn btn-dark">Acceder</button>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     </section>
-
-
   </div>
 </template>
 
-
-<script>
-
-</script>
+<script></script>

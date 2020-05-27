9e04a3381270b2224cc2f9d5ba1d5f407121937d
Omar97perez <omarperezznakar@gmail.com>
2020-05-27 21:19:03 +0100

    Servidor: dispone 3 algoritmos (Clustering, Regresión y Clasificación)

--- server/index.js
+++ server/index.js
@@ -10,11 +10,26 @@ let fs = require('fs');
 var request = require('request');
 const axios = require('axios')
 
-
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
+
+function executeMake(nameMethod){
+	require('child_process').execSync("sudo make -C ./Algoritmos/" + nameMethod + " all");
+}
+
+// Permite Subir Imagenes
+let storage = multer.diskStorage({
+	destination:(req, file, cb) => {
+		cb(null, './Archivos')
+	},
+	filename: (req, file, cb) => {
+		cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+	}
+});
+
+const upload = multer({storage});
 
 //Conexión con la base de datos, cuando se despliegue en servidor  se tendrá que cambiar la dirección
 mongoose.connect('mongodb://omar:antonio1997@cluster0-shard-00-00-svm5b.mongodb.net:27017,cluster0-shard-00-01-svm5b.mongodb.net:27017,cluster0-shard-00-02-svm5b.mongodb.net:27017/TrabajoFinalCN?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true')
@@ -29,3 +44,30 @@ mongoose.connect('mongodb://omar:antonio1997@cluster0-shard-00-00-svm5b.mongodb.
   app.listen(app.get('port'), () =>{
       console.log('Server on port', app.get('port'));
   });
+
+// Permite Ejecutar Métodos 
+app.post('/Execute/Algorithm/:name/:Elements', upload.single('file'), (req, res) => {
+
+  var elementsUrl = req.params.Elements.split("-");
+  var fileExit = req.file.filename.split(".");
+
+  fs.readFile("./Metodos.json", 'utf-8', (err2, data) => {
+    var methods = JSON.parse(data);
+    var element = methods['Methods'].findIndex(method => method.Name === req.params.name);
+    var method = methods['Methods'][element];
+    executeMake(method["Name"]);	
+    var elements = method["Elements"];
+    var stringFinal = "";
+
+    if (elementsUrl.length != 1){
+      for(x =0; x < elementsUrl.length; x++){
+        stringFinal += " " + elements[x]["Name"] + "=" + elementsUrl[x];
+      }	
+    }
+
+    const exec = require('child_process').exec;
+    exec("make -C ./Algoritmos/" + req.params.name + " file=../../Archivos/"+ req.file.filename + " fileExit=../../Archivos/" + fileExit[0] + ".png " + stringFinal + " run", (err, stdout, stderr) => {
+      res.send(req.file.filename);
+    });
+  });	
+});

d60072c9c2f6a8f83b7d37a604d71c65b86aaa80
Omar97perez <omarperezznakar@gmail.com>
2020-05-27 21:41:12 +0100

    Cliente: ejecuta Scikit Learn Básico

--- client/app/components/productos/ScikitLearn.vue
+++ client/app/components/productos/ScikitLearn.vue
@@ -1,5 +1,76 @@
 <template>
   <div>
+    <!-- Modal Métodos -->
+      <div class="modal fade" id="ModalEjecutarMetodo" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+          <div class="modal-content">
+              <div class="modal-header">
+              <h3 class="modal-title" id="TitleMethod"></h3>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+              </button>
+              </div>
+              <div class="modal-body" align="center">
+                  <form id="ModalMetodo"></form>
+                  <form class="mt-3" align="left">
+                      <label class="font-weight-bold">Archivo</label>
+                  </form>
+                  <input type="file" id="fileExecuteMethod">
+              </div>
+                  <div class="modal-footer">
+                  <button type="button" class="btn btn-dark" @click="DescargarFicheroPrueba()">Descargar Archivo Prueba</button>
+                  <button type="button" class="btn btn-primary" data-dismiss="modal" @click="EjecutarAlgoritmo()">Enviar</button>
+                  <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+              </div>
+          </div>
+          </div>
+      </div>
+
+      <!-- Modal Cargar Imagen -->
+      <div class="modal fade bd-example-modal-lg" id="ModalCargaImagen" role="dialog" aria-labelledby="exampleModalLabel">
+          <div class="modal-dialog modal-lg" role="document">
+              <div class="modal-content">
+                  <div class="modal-header">
+                      <h3 class="modal-title" id="exampleModalLabel">Visualizar Imagen</h3>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                      </button>
+                  </div>
+                  <div class="modal-body" >
+                      <div class="d-flex justify-content-center">
+                          <div class="spinner-border" role="status">
+                              <span class="sr-only">Loading...</span>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="modal-footer">
+                      <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+
+      <!-- Modal Ver Imágenes -->
+      <div class="modal fade bd-example-modal-lg" id="ModalVerImagen" role="dialog" aria-labelledby="exampleModalLabel">
+          <div class="modal-dialog modal-lg" role="document">
+              <div class="modal-content">
+                  <div class="modal-header">
+                      <h3 class="modal-title" id="exampleModalLabel">Visualizar Imagen</h3>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                          <span aria-hidden="true">&times;</span>
+                      </button>
+                  </div>
+                  <div class="modal-body" align="center">
+                      <img class="img-fluid mt-3" id="myimage"/>
+                  </div>
+                  <div class="modal-footer">
+                      <button type="button" class="btn btn-primary" id="prueba" rel="myimage" @click="DescargarImagen()">Descargar</button>
+                      <button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+                  </div>
+              </div>
+          </div>
+      </div>
+
     <section class="intro-single">
       <div class="container">
         <div class="row">
@@ -21,7 +92,7 @@
               <h5 class="card-title">Scikit Learn</h5>
               <p class="card-text">Use la librería de Scikit Learn en la nube.</p>
               <div class="card-footer mt-2" align="center" style="position: relative!important; border-top: 1px solid  #343a40!important;">
-                <button class="btn btn-dark">Acceder</button>
+                <button class="btn btn-dark" data-toggle="modal" data-target="#ModalEjecutarMetodo" @click="CargarFormulario('ScikitLearn')">Acceder</button>
               </div>
             </div>
           </div>
@@ -38,7 +109,7 @@
               <h5 class="card-title">Scikit Learn</h5>
               <p class="card-text">Use la librería de Scikit Learn en la nube.</p>
               <div class="card-footer mt-2" align="center" style="position: relative!important; border-top: 1px solid  #343a40!important;">
-                <button class="btn btn-dark">Acceder</button>
+                <button class="btn btn-dark" data-toggle="modal" data-target="#ModalEjecutarMetodo" @click="CargarFormulario('ScikitLearn')">Acceder</button>
               </div>
             </div>
           </div>
@@ -55,7 +126,7 @@
               <h5 class="card-title">Scikit Learn</h5>
               <p class="card-text">Use la librería de Scikit Learn en la nube.</p>
               <div class="card-footer mt-2" align="center" style="position: relative!important; border-top: 1px solid  #343a40!important;">
-                <button class="btn btn-dark">Acceder</button>
+                <button class="btn btn-dark" data-toggle="modal" data-target="#ModalEjecutarMetodo" @click="CargarFormulario('ScikitLearn')">Acceder</button>
               </div>
             </div>
           </div>
@@ -66,4 +137,110 @@
   </div>
 </template>
 
-<script></script>
+<script>
+
+import axios from 'axios';
+class Buscador {
+  constructor(busqueda = '',tipo = '',ciudad = '') {
+    this.busqueda = busqueda;
+  }
+}
+export default {
+  name: 'clases',
+  data() {
+    return {
+      Productos: [],
+      Buscador: new Buscador(),
+      busqueda: '',
+      ciudad: '',
+      tipo:'clases',
+      titleMethod:'',
+      Paginacion: [],
+    }
+  },
+  created() {
+    this.getProductos();
+  },
+  methods: {
+    getProductos() {
+      fetch('http://localhost:3000/api/Get/Methods')
+        .then(res => res.json())
+        .then(data => {
+          this.Paginacion = data["Methods"];
+          this.Productos = this.Paginacion.slice(0,this.tampagina);
+        });
+    },
+    CargarFormulario(title){
+      this.titleMethod = title;
+      var position = this.Paginacion.findIndex(method => method.Name === title);
+      document.getElementById('TitleMethod').innerHTML = this.Paginacion[position].Name;
+      document.getElementById('ModalMetodo').innerHTML = "";
+      for (var x=0;x<(Object.keys(this.Paginacion[position].Elements).length);x++) { 
+          document.getElementById('ModalMetodo').innerHTML += '<div class="mt-3 form-group" align="left"><label class="font-weight-bold" for="exampleInputEmail1">'+ this.Paginacion[position].Elements[x].Name + "2" + '</label><div class="input-group mb-3"><input type="text" name="' + this.Paginacion[position].Elements[x].Name + '" id="' + this.Paginacion[position].Elements[x].Name + "2" + '" value="' + this.Paginacion[position].Elements[x].value + '" class="form-control"> <div class="input-group-append"><button class="btn btn-outline-primary" type="button" data-toggle="collapse" href="#'+ this.Paginacion[position].Elements[x].Name +'" role="button" aria-expanded="false" aria-controls="collapseExample"><i class="fa fa-question-circle" aria-hidden="true"></i></button></div></div><small id="emailHelp" class="form-text text-muted">'+ this.Paginacion[position].Elements[x].DescriptionShort +'</small></div> <div class="collapse" id="' + this.Paginacion[position].Elements[x].Name +'"><div class="card card-body">'+ this.Paginacion[position].Elements[x].DescriptionLong +'</div></div>';
+      }
+    },
+    Endrefresh()
+    {
+        document.getElementById("loading").className = "";
+        location.reload(true);
+    },
+    EjecutarAlgoritmo(){
+      $("#ModalCargaImagen").modal();
+      var position = this.Paginacion.findIndex(method => method.Name === this.titleMethod);
+      var elements = "";
+      for (var x=0;x<(Object.keys(this.Paginacion[position].Elements).length);x++) { 
+          if(x != 0){
+              elements +=  "-";
+          }
+          elements += document.getElementById(this.Paginacion[position].Elements[x]["Name"] + "2").value;
+      }
+
+      var urlPostMetodo = "";
+
+      if(elements == ""){
+          urlPostMetodo = 'http://localhost:3000/Execute/Algorithm/' + this.titleMethod + '/' + "no";
+      }
+      else{
+          urlPostMetodo = 'http://localhost:3000/Execute/Algorithm/' + this.titleMethod + '/' + elements;
+      }
+      var formData = new FormData();
+      formData.append("file", document.getElementById("fileExecuteMethod").files[0]);
+
+      $.ajax({
+          url: urlPostMetodo,
+          type: "POST",
+          data: formData,
+          processData: false,
+          contentType: false,
+          success: function(response) {
+              $("#ModalCargaImagen").modal('hide');
+              $('body').removeClass('modal-open');
+              $('.modal-backdrop').remove();
+              response = response.split(".");
+              document.getElementById('myimage').src = 'http://localhost:3000/Get/file/' + response[0] + ".png";
+              $('body').removeClass('ModalCargaImagen');
+              $("#ModalVerImagen").modal();
+          },
+          error: function(jqXHR, textStatus, errorMessage) {
+              console.log(errorMessage); 
+          }
+      });
+    },
+    DescargarImagen() {
+        var nameHC = document.getElementById('myimage');
+        var link = document.createElement("a");
+        link.download = "image.png";
+        link.href = nameHC.src;
+        link.click();
+    },
+    DescargarFicheroPrueba() 
+    {
+      var link = document.createElement("a");
+      var position = this.Paginacion.findIndex(method => method.Name === this.titleMethod);
+      link.download = this.Paginacion[position].file;
+      link.href = "/api/Get/file/" + this.Paginacion[position].file;
+      link.click();
+    }
+  },
+};
+</script>

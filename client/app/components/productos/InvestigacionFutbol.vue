586a4a145633b18c26e38a05e666a490c6de81eb
Omar97perez <omarperezznakar@gmail.com>
2020-06-10 16:08:12 +0100

    Servidor: Código ADM

--- client/app/components/productos/InvestigacionFutbol.vue
+++ client/app/components/productos/InvestigacionFutbol.vue
@@ -9,6 +9,55 @@
             </div>
           </div>
         </div>      
+        <div class="mt-5 font-weight-bold" align="center">
+          <h1>Introducción</h1>
+        </div>
+        <div class="mt-5" >
+          <p style="font-size:20px;">
+            Este proyecto consiste en un estudio sobre cómo podremos predecir la evolución de un equipo de fútbol en su liga. Para poder realizar esta acción, es necesario disponer de algunos datos de la liga actual (resultados de los últimos partidos,..).
+          </p> 
+          <p style="font-size:20px;">
+            En este proyecto, también podremos ver como iremos probando con diferentes algoritmos de machine learning para intentar encontrar el algoritmo perfecto para este tipo de problema. De igual forma, estos se irán subiendo al proyecto realizado en la asignatura denominada “Computación en la Nube” en el Framework denominado “GraphCloud”.
+          </p>
+          <p style="font-size:20px;">
+            Por último, cabe resaltar que se intentará sacar unas conclusiones finales sobre el estudio para que nos sirva para poder aplicarlas a un equipo de fútbol en concreto (si se desease).
+          </p>
+        </div>
+
+        <div class="mt-5 font-weight-bold" align="center">
+          <h1>Ejecución</h1>
+        </div> 
+
+        <div class="row mt-5">
+          <div class="col-1"></div>
+          <div class="card col-4" style="border: 1px solid #343a40!important">
+            <img class="card-img-top" src="/img/futbol.jpeg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Ejecutar previsión de fútbol</h5>
+              <p class="card-text">Esta opción nos permite poder predecir el puesto que va a logarr un equipo sabiendo los 10 primeros partidos jugados.</p>
+              <div class="card-footer mt-2" align="center" style="position: relative!important; border-top: 1px solid  #343a40!important;">
+                  <router-link :to="{ name: 'InvestigacionFutbol' }" >
+                    <button class="btn btn-dark">Acceder</button>
+                  </router-link>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-2"></div>
+
+          <div class="card col-4" style="border: 1px solid #343a40!important">
+            <img class="card-img-top" src="/img/Excel.jpeg" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Ejecutar con otra fuente de datos</h5>
+              <p class="card-text">Esta opción nos permite poder ejecutar un algoritmo de predicción para una fuente propia.</p>
+              <div class="card-footer mt-2" align="center" style="position: relative!important; border-top: 1px solid  #343a40!important;">
+                  <router-link :to="{ name: 'InvestigacionFutbol' }" >
+                    <button class="btn btn-dark">Acceder</button>
+                  </router-link>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   </div>
---
+++ server/Algoritmos/ADM/main.py
@@ -0,0 +1,96 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import StrategyFile as sf
+import sys
+import string
+import os
+import geopandas as gpd
+import numpy as np
+from sklearn import datasets, linear_model
+from sklearn.linear_model import LinearRegression, RANSACRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.neural_network import MLPRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_predict, train_test_split
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+from sklearn import model_selection
+from pandas.plotting import scatter_matrix
+from sklearn.cluster import MeanShift, estimate_bandwidth
+from sklearn.datasets import make_blobs
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from scipy.cluster.hierarchy import dendrogram
+from sklearn.datasets import load_iris
+from sklearn.cluster import AgglomerativeClustering
+from sklearn.cluster import KMeans
+from time import time
+
+pedirParametros = int(sys.argv[2])
+
+#Cargamos los datos de un fichero
+file = sys.argv[1]
+fichero = os.path.splitext(file)
+fichero = fichero[0] + ".csv"
+nombreFichero = ""
+
+if file.endswith('.csv'):
+    fileSelected = sf.Csv(file, fichero)
+    df = fileSelected.collect()
+elif file.endswith('.json'):
+    fileSelected= sf.Json(file, fichero)
+    df = fileSelected.collect()
+elif file.endswith('.xlsx'):
+    fileSelected= sf.Xlsx(file, fichero)
+    df = fileSelected.collect()
+else:
+    print("Formato no soportado")
+    sys.exit()
+
+
+if(pedirParametros == 1):
+    algoritmoSeleccionado = int(input('¿Qué algoritmo quiere ejecutar?: \n\t 1. Regresión Lineal. \n\t 2. Árbol de Regresión. \n\t 3. Regresión árbol Aleatorio. \n\t 4. Red Neuronal.\n  > '))
+    columnaSeleccionadaInicial = int(input('¿Qué columna inicial quiere analizar?\n > '))
+    columnaSeleccionada = int(input('¿Qué columna final quiere analizar?\n > '))
+    valoresPredecir = input('¿Qué valores tiene para predecir?\n > ')
+else:
+    algoritmoSeleccionado = int(sys.argv[3])
+    columnaSeleccionadaInicial = int(sys.argv[4])
+    columnaSeleccionada = int(sys.argv[5])
+    valoresPredecir = sys.argv[6]
+    rutaEscribirJson = sys.argv[7]
+
+array = df.values
+X = (array[:,columnaSeleccionadaInicial:columnaSeleccionada])
+Y = (array[:,columnaSeleccionada])
+
+if algoritmoSeleccionado == 1:
+    model = LinearRegression()
+elif algoritmoSeleccionado == 2:
+    model = DecisionTreeRegressor()
+elif algoritmoSeleccionado == 3:
+    model = RandomForestRegressor()
+elif algoritmoSeleccionado == 4:
+    model = MLPRegressor()
+else:
+    print("El algoritmo introducido no existe")
+    sys.exit()
+
+valorSplit = valoresPredecir.split(",")
+valorMap = list(map(float, valorSplit))
+
+valoresPredecir = np.array([valorMap])
+# valorPredecir = np.array([[1,1,2010,1.00,12.00,5.00,31.00,56.00,0.4,2.00,3.00,2.00]])
+reg = model.fit(X, Y)
+result = reg.predict(valoresPredecir)
+
+if(pedirParametros == 1):
+    print("Result: \n")
+    print(result)
+else:
+    json = '{"Resultado":'+ str(result[0]) +'}'
+    file = open(rutaEscribirJson + fichero[0] + ".json", "w")
+    file.write(json)

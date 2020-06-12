06bf3205880252dd761e10d4439251894d8a243b
PierreSimT <alu0101371030@ull.edu.es>
2020-06-12 12:26:58 +0100

    Merge y cambios preparados para su despliegue en Google

--- client/app/components/productos/InvestigacionFutbol.vue
+++ client/app/components/productos/InvestigacionFutbol.vue
@@ -169,7 +169,7 @@ export default {
   },
   methods: {
     getProductos() {
-      fetch('http://localhost:3000/api/Get/Methods')
+      fetch('/api/Get/Methods')
         .then(res => res.json())
         .then(data => {
           this.Paginacion = data["Methods"];
@@ -206,10 +206,10 @@ export default {
       var urlPostMetodo = "";
 
       if(elements == ""){
-          urlPostMetodo = 'http://localhost:3000/api/Execute/Algorithm/' + this.titleMethod + '/' + "no";
+          urlPostMetodo = '/api/Execute/Algorithm/' + this.titleMethod + '/' + "no";
       }
       else{
-          urlPostMetodo = 'http://localhost:3000/api/Execute/Algorithm/' + this.titleMethod + '/' + elements;
+          urlPostMetodo = '/api/Execute/Algorithm/' + this.titleMethod + '/' + elements;
       }
       var formData = new FormData();
       formData.append("file", document.getElementById("fileExecuteMethod").files[0]);
@@ -226,7 +226,7 @@ export default {
               $('body').removeClass('modal-open');
               $('.modal-backdrop').remove();
               response = response.split(".");
-              document.getElementById('myimage').src = 'http://localhost:3000/api/Get/file/' + response[0] + ".png";
+              document.getElementById('myimage').src = '/api/Get/file/' + response[0] + ".png";
               $('body').removeClass('ModalCargaImagen');
               $("#ModalVerImagen").modal();
           },
@@ -247,7 +247,7 @@ export default {
       var link = document.createElement("a");
       var position = this.Paginacion.findIndex(method => method.Name === this.titleMethod);
       link.download = this.Paginacion[position].file;
-      link.href = "http://localhost:3000/api/Get/file/" + this.Paginacion[position].file;
+      link.href = "/api/Get/file/" + this.Paginacion[position].file;
       link.click();
     }
   },
